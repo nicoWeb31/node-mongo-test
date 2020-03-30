2ce23a43a657f3843c9fbe6c93e9aa4e5d88cae2
Riot Nicolas <nico.riot@free.fr>
2020-03-30 12:48:27 +0200

    installation mogoose test recup donner

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-let express = require("express");
-let serv = express();
-let morgan = require("morgan");
-let router = require("./router");
+const express = require("express");
+const serv = express();
+const morgan = require("morgan");
+const router = require("./router");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/biblio",{useNewUrlParser:true,useUnifiedTopology:true});
+const livresShema = mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    titre:String,
+    autheur:String,
+    nbrePages:Number
+});
+const livresModel = mongoose.model("livre",livresShema);
+livresModel.find()
+    .exec()
+    .then(livres => 
+        console.log(livres)
+        )
+    .catch();    
+
 
 serv.use(express.static("public"));
 serv.use(morgan("dev"));
